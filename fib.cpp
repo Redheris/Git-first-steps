f64e5d92f7b4649355c309ac96cf01695c258532
Anatoly Solomonov <solomonov010@mail.ru>
2022-02-24 01:25:29 +0400

    Added recursive function to find Fibonacci number

--- fib.cpp
+++ fib.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+int fib (int n){
+	if (n == 0)
+		return 1;
+	if (n == 1)
+		return 1;
+	return fib(n - 1) + fib(n - 2);
+}
+
 int main(){
+	int n;
+
 	cout << "Hello, World!" << endl;
+
+	cout << "n = "; cin >> n;
+	cout << "Fib(" << n << ") = " << fib(n-1) << endl;
 }
